0af456ffebc5e329089abe20fefba23ffebac940
Albin Sunny <psalbin2005@gmail.com>
2025-03-26 22:10:52 +0530

    QuestionService HttpStatus Added

--- src/main/java/com/learn/quizApp/service/QuestionService.java
+++ src/main/java/com/learn/quizApp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.learn.quizApp.service;
 import com.learn.quizApp.model.Question;
 import com.learn.quizApp.repository.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class QuestionService {
@@ -12,16 +15,25 @@ public class QuestionService {
     @Autowired
     QuestionRepo questionrepo;
 
-    public List<Question> getAllQuestions() {
-       return questionrepo.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionrepo.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionrepo.findByCategory(category);
+    public ResponseEntity<List<Question>>getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionrepo.findByCategory(category),HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
          questionrepo.save(question);
-         return "success";
+         return new ResponseEntity<>("success",HttpStatus.CREATED);
     }
 }
