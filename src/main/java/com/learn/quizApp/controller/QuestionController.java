b655ec01cc35f09e363fd79c952f9c0724e69f97
Albin Sunny <psalbin2005@gmail.com>
2025-03-26 21:11:22 +0530

    InitalCommit

--- src/main/java/com/learn/quizApp/controller/QuestionController.java
+++ src/main/java/com/learn/quizApp/controller/QuestionController.java
@@ -25,6 +25,7 @@ public class QuestionController {
     }
 
 
+
     @PostMapping("add")
     public String addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
