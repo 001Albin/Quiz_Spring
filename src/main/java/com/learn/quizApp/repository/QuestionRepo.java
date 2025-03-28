e8737a165f0485d128e980b7bec89d0c6388ddd5
Albin Sunny <psalbin2005@gmail.com>
2025-03-28 20:35:47 +0530

    Folder for Quiz Ready

--- src/main/java/com/learn/quizApp/repository/QuestionRepo.java
+++ src/main/java/com/learn/quizApp/repository/QuestionRepo.java
@@ -2,6 +2,7 @@ package com.learn.quizApp.repository;
 
 import com.learn.quizApp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 @Repository
 public interface QuestionRepo extends JpaRepository<Question,Integer> {
     List<Question> findByCategory(String category);
+
+    @Query(value = "select * from question q where q.category=:category ORDER BY RANDOM() LIMIT :noOfQuestions",nativeQuery = true)
+
+    List<Question> findRandomQuestionsByCategory(String category, int noOfQuestions);
 }
