dc80f5ff3481d759dc3dc7a757769691d9ce77e0
Albin Sunny <psalbin2005@gmail.com>
2025-03-28 21:09:56 +0530

    Removed unnecessary files from the root directory

--- src/main/java/com/learn/quizApp/repository/QuizRepo.java
+++
@@ -1,7 +0,0 @@
-package com.learn.quizApp.repository;
-
-import com.learn.quizApp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepo extends JpaRepository<Quiz,Integer> {
-}
